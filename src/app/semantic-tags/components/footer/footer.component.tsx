ee5da9fd8772463bf17c5acce04d79e0be4bc11e
BijanProgrammer <bijaneisapour@gmail.com>
2026-08-07 18:46:09 +0330

    [UPDATE] hardcoded year to dynamic year

--- src/app/semantic-tags/components/footer/footer.component.tsx
+++ src/app/semantic-tags/components/footer/footer.component.tsx
@@ -3,9 +3,11 @@ import { ReactElement } from "react";
 import styles from "./footer.module.css";
 
 export default function FooterComponent(): ReactElement {
+  const year = new Date().getFullYear();
+
   return (
     <footer id="footer" className={styles.footer}>
-      Codective &copy; 2025 All rights reserved
+      Codective &copy; {year} All rights reserved
     </footer>
   );
 }
